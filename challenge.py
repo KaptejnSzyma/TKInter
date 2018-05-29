f4848fa3e893879a6623f4070ce383c694460041
Szymek <szymon.rzadca@gmail.com>
2018-05-29 11:47:12 +0200

    tkinter challenge enhanced

--- challenge.py
+++ challenge.py
@@ -8,23 +8,15 @@ mainWindow.title('Calculator')
 mainWindow.geometry('200x200')
 mainWindow.resizable(False, False)
 mainWindow['padx'] = 10
-buttons1 = ['CE', 'C']
-buttons2 = ['7', '8', '9', '+']
-buttons3 = ['4', '5', '6', '-']
-buttons4 = ['1', '2', '3', '*']
-buttons5 = ['0', '=', '/']
-result = tkinter.Entry(mainWindow)
-result.grid(row=0, column=1,  columnspan=3, sticky='nsew')
 
-for b in range(len(buttons1)):
-    tkinter.Button(mainWindow, text=buttons1[b]).grid(row=1, column=b)
-for b in range(len(buttons2)):
-    tkinter.Button(mainWindow, text=buttons2[b]).grid(row=2, column=b)
-for b in range(len(buttons3)):
-    tkinter.Button(mainWindow, text=buttons3[b]).grid(row=3, column=b)
-for b in range(len(buttons4)):
-    tkinter.Button(mainWindow, text=buttons4[b]).grid(row=4, column=b)
-for b in range(len(buttons5)):
-    tkinter.Button(mainWindow, text=buttons5[b]).grid(row=5, column=b)
+buttons = [['CE', 'C'], ['7', '8', '9', '+'], ['4', '5', '6', '-'], ['1', '2', '3', '*'], ['0', '=', '/']]
+
+result = tkinter.Entry(mainWindow)
+result.grid(row=0, column=1,  columnspan=5, sticky='nsew')
+
+for r in range(0, 5):
+    for b in range(len(buttons[r])):
+        tkinter.Button(mainWindow, text=buttons[r][b]).grid(row=r+1, column=b)
+
 
 mainWindow.mainloop()
